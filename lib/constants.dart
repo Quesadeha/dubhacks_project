d37b9e914fd61563d4cbb492d0eada7b68343611
Quesadeha <deja.j.lewis03@gmail.com>
2022-10-22 11:22:08 -0700

    silly little app

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+const titleColor = Color.fromARGB(255, 50, 50, 50);
+
+const normalTextColor = Color.fromARGB(255, 100, 100, 100);
---
+++ lib/home_screen/home_screen_body.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenBody extends StatefulWidget {
+  const HomeScreenBody({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreenBody> createState() => _HomeScreenBodyState();
+}
+
+class _HomeScreenBodyState extends State<HomeScreenBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          Text(
+            "Beginning to our hackathon", 
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.red
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
